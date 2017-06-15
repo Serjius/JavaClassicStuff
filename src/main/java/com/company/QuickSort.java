7771be3b1139b601dd1a4bdb60d8f4960ab37b98
Serge Puchnin <sergey.puchnin@gmail.com>
2017-06-15 22:43:28 +0300

    code is ready

--- src/main/java/com/company/QuickSort.java
+++ src/main/java/com/company/QuickSort.java
@@ -2,12 +2,50 @@ package com.company;
 
 /**
  * Created by pss on 15.06.17.
- *
+ * <p>
  * try to remember QS
  */
-public class QuickSort<T extends Comparable<T>>  {
+public class QuickSort {
 
+    public static void sort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
 
+    private static void quickSort(int[] array, int start, int finish) {
+        if (start < finish) {
+            int pivot = array[(start + finish) / 2];
+            int left = start;
+            int right = finish;
+            while (left <= right) {
+                while (array[left] < pivot) {
+                    left++;
+                }
+                while (array[right] > pivot) {
+                    right--;
+                }
+
+                if (left <= right) {
+                    swap(array, left, right);
+                    left++;
+                    right++;
+                }
+
+            }
+            quickSort(array, start, right);
+            quickSort(array, left, finish);
+
+        }
+
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+
+    }
+
+    ;
 
 
 }
